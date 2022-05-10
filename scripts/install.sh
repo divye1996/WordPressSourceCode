1827fcdf20dcb97a92c67322057fa9d174ff3df3
divye1996 <56021140+divye1996@users.noreply.github.com>
2022-05-10 12:58:46 +0530

    Create install.sh

---
+++ scripts/install.sh
@@ -0,0 +1,8 @@
+!/bin/bash
+apt -y update
+apt install -y ruby
+apt install -y awscli
+cd /home/ubuntu
+wget https://aws-codedeploy-ap-south-1.s3.ap-south-1.amazonaws.com/latest/install
+chmod +x ./install
+./install auto
